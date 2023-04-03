3c11c5da881c459b28b793f5fa40114689d955b8
Sunattulo <muslimamuminova6969@gmail.com>
2023-04-03 22:47:58 +1200

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { Modal } from "./components/Modal/Modal";
+import { Table } from "./components/Table/Table";
+
+function App() {
+  return (
+    <div>
+      <Table />
+      <Modal />
+    </div>
+  );
+}
+
+export default App;
